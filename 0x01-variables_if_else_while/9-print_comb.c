da92680870f7e30a0f93bfba86312e272eb6e810
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-14 10:41:56 -0700

    printing all number 0 - 9 separated by a comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all number 0 - 9 separated by a comma
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		if (number < 57)
+			putchar(',');
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
