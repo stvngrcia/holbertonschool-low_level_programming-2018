73af66c03b7ba39c336284f231d113fdef597da2
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-14 10:56:32 -0700

    printing combinations 0 - 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all number 00 - 99  separated by a comma
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	int number1;
+
+	number = 48;
+	while (number <= 57)
+	{
+		number1 = 48;
+		while (number1 <= 57)
+		{
+			putchar(number);
+			putchar(number1);
+			number1++;
+			if (number < 57 || number1 <= 57)
+				putchar(',');
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
