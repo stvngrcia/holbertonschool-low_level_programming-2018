551950b0fae5270204028e56e824064164a9698a
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-14 12:17:22 -0700

    printing unique combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing unique combinations
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	int number;
+	int number1;
+
+	number = 48;
+	while (number <= 57)
+	{
+		number1 = 0;
+		while (number1 <= 57)
+		{
+			if (number < number1)
+			{
+				putchar(number);
+				putchar(number1);
+				if (number < 56)
+					putchar(',');
+			}
+			number1++;
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
