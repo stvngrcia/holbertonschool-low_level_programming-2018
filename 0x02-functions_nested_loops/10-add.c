a2586859c09e78a0de4a278c5db6e4c1ff9e56d1
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-18 16:42:03 -0700

    adding numebers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add- adds two numbers
+ * @x: number
+ * @y: number
+ * Description: adds two integers
+ * Return: the sum of two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
