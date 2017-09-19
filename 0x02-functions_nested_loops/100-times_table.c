2c2ef6e4bf73473682cd1c6a4cda30ed1a795be4
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-18 23:12:22 -0700

    printing the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,74 @@
+#include "holberton.h"
+
 /**
- * times_table- prints the times table
+ * print_times_table- prints the times table
+ * @z: limit for multiplication
  */
 
-void print_times_table(int n)
+void print_times_table(int z)
 {
 	int n;
 	int m;
 	int mult;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n <= z; n++)
 	{
-		for (m = 0; m <= 9; m++)
+		for (m = 0; m <= z; m++)
 		{
-			mult = (m * n);
-			if (mult <= 9)
+			if (z < 15 && z > 0)
 			{
-				_putchar(mult + 48);
+				mult = (m * n);
+				operation(mult);
+				spaces(n, m, z);
 			}
-			else
-			{
-				_putchar(mult / 10 + 48);
-				_putchar(mult % 10 + 48);
-			}
-			if (m < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if ((n * (m + 1)) <= 9 &&  m != 9)
-				_putchar(' ');
 		}
-		_putchar('\n');
+		if (z < 15 && z > 0)
+			_putchar('\n');
+	}
+}
+
+/**
+ * operation- Prints the numbers character by character
+ * @mult: multiplication
+ */
+void operation(int mult)
+{
+	if (mult <= 9)
+	{
+		_putchar(mult + 48);
+
+	}
+	else if (mult <= 99)
+	{
+		_putchar(mult / 10 + 48);
+		_putchar(mult % 10 + 48);
+	}
+	else if (mult <= 999)
+	{
+		_putchar(mult / 100 + 48);
+		_putchar((mult / 10) % 10 + 48);
+		_putchar(mult % 10 + 48);
+	}
+}
+/**
+ * spaces- Decides when to put spaces in between numbers
+ *@n: Number to multiply by collum
+ *@m: Number to multiply by row
+ *@z: Limit of number
+ */
+void spaces(int n, int m, int z)
+{
+	if (m < z)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	if (n * (m + 1) <= 99 && m < z)
+	{
+		_putchar(' ');
+	}
+	if (n * (m + 1) <= 9 && m < z)
+	{
+		_putchar(' ');
 	}
 }
