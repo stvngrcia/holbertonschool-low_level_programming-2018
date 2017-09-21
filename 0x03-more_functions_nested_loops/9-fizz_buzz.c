557747b345348ad8d688999225f0188f09b5b583
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-21 12:09:46 -0700

    adding a return comment

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,6 +5,7 @@
  * followed by a new line. But for multiples of three print
  * Fizz instead of the number and for the multiples of five print Buzz.
  * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: Always 0.
  */
 
 int main(void)
