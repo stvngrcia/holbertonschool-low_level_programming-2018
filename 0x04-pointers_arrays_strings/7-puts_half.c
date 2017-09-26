fe0f216f4d15c1f0bf486df896539075ca62aac4
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-26 09:03:17 -0700

    testing

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -14,10 +14,7 @@ void puts_half(char *str)
 
 	half_len = len / 2;
 
-	if (len > 0)
-	{
-		for (; half_len < len; half_len++)
-			_putchar(str[half_len]);
-		_putchar('\n');
-	}
+	for (; half_len < len; half_len++)
+		_putchar(str[half_len]);
+	_putchar('\n');
 }
