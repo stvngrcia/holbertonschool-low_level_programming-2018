5d5b831f12d5dd766dcdffe2be81d41b6899c9d2
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-26 08:06:45 -0700

    fixing comments

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
-/*
- *_strlen - Calculates the lenght of a string
+/**
+ * _strlen - Calculates the length of a string
  * @s: string
+ * Descripion: Calculates the lenght of a string
  * Return: An integer representing the lenght of a string
  */
 int _strlen(char *s)
