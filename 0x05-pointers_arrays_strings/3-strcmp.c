d1e5ee4037400146b4dac9ea2aa64c3927955e63
Steven Garcia <steven.garcia@holbertonschool.com>
2017-09-28 09:15:32 -0700

    _strcmp - compares two strings

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
 }
