73036f679ea39e71ea1429e88e36721cae8b724c
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-02 09:08:25 -0700

    _strchr - returns a pointer to the first occurrence of the character c

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - returns a pointer to the first occurrence of the character c
+ * in the string s.
+ * @s: string
+ * @c: Character
+ * Return: pointer to the first occurence of the character or pointer to null
+ * value if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ In this folder the following functions can be found
 
 + _memset - fill memory with a constant byte
 + _memcpy - copy memory area
++ _strchr - returns a pointer to the first occurrence of the character c
