811b749fb1966e3b93d64beba29ca9527b16d132
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-02 08:53:35 -0700

    _memset - fill memory with a constant byte

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+#Synopsis
+In this folder the following functions can be found
+
++ _memset - fill memory with a constant byte
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif
