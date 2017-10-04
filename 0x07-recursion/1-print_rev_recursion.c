e4febbb3f040c2e54e9af9d50fb3b93f923353b5
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-04 09:38:48 -0700

    _print_rev_recursion - pritns a  reversed string

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+char *rev(char *s);
+
+/**
+ * _print_rev_recursion - pritns a  reversed string
+ * @s: String to reverse
+*/
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\n')
+		s = rev(s);
+	else if (*(s - 1) == '\n')
+	{
+		_putchar(*s);
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s--;
+	_print_rev_recursion(s);
+}
+
+/**
+ * rev - Reverses a string
+ * @s: String to reverse
+ * Return: Pointer to a string
+*/
+char *rev(char *s)
+{
+	s++;
+	if (*(s + 1) == '\0')
+		return (s);
+	return (rev(s));
+}
