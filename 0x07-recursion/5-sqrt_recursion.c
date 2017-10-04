1c30f548d022ba1d1eb5c9de12f58363a51ab1cd
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-04 13:21:42 -0700

    adding function to calculate the square root of a number

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+int calculation(int n, int mult);
+
+/**
+ * _sqrt_recursion - Calculates the square root of a number
+ * @n: Number to calculate the square root of
+ * Return: The square root of a number
+ */
+int _sqrt_recursion(int n)
+{
+	int mult;
+
+	mult = 1;
+	mult = calculation(n, mult);
+	return(mult);
+}
+
+/**
+ * calculation - Calculates the square root of a number
+ * @n: Number to calculate the square root of
+ * @mult: Keeps track of the numbers
+ * Return: The square root of a number
+ */
+int calculation(int n, int mult)
+{
+	if (n < 0)
+		return(-1);
+	if (mult * mult == n)
+		return(mult);
+	if (mult > n)
+		return(-1);
+	return(calculation(n, mult + 1));
+}
