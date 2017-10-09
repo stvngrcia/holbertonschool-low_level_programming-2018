e0974b730376565bcc68b8cdd4c3def02ad32e91
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-08 18:12:09 -0700

    adding a function to check for non number parameters

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+int isnumber(char *argument);
 
 /**
  * main - Entry point
@@ -23,7 +24,8 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+
+		if (isnumber(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
@@ -31,5 +33,24 @@ int main(int argc, char **argv)
 		sum = sum +  atoi(argv[i]);
 	}
 	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * isnumber - checks if there is not a numeber on the string
+ * @argument: argument string
+ * Return: 0 if not letters are found returns 1 if there are letters
+ */
+int isnumber(char *argument)
+{
+	int i;
+
+	for (i = 0; argument[i] != '\0'; i++)
+	{
+		if (argument[i] < '0' ||  argument[i] > '9')
+		{
+			return (1);
+		}
+	}
 	return (0);
 }
