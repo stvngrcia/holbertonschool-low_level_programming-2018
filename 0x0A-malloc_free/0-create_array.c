3e0aaff9343c33708bfe723939dc29c0114923ef
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-10 09:39:13 -0700

    function that allocates necessary memory for an array of character

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * create_array - Creates space in memory for an array
+ * @size: the size of the array
+ * @c: A character to initilize the array
+ * Return: A pointer to the space allocated
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *heap_array;
+
+	heap_array = malloc(size);
+	if (heap_array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		heap_array[i] = c;
+	}
+	return (heap_array);
+}
