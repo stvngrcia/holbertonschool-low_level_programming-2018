5aa4bb6c5a1f97d11b2048090b942c9dadeb42a9
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-10 09:49:58 -0700

    Function that duplicates a string

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * _strdup - Duplicates a string using malloc
+ * @str: String to be duplicated
+ * Return: NULL if not enough space in memory otherwise a pointer
+ * to the beginning of the array
+ */
+
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char *heap_array;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{}
+	heap_array = malloc(len);
+	if (heap_array == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		heap_array[i] = str[i];
+	}
+	return (heap_array);
+}
