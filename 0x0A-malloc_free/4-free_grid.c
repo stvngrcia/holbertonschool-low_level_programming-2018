a14b87f039f85347f6c1e3d754c0280f54d30fe0
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-10 13:23:00 -0700

    function that frees an array

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 
+/**
+ * free_grid - Freeing a grid
+ * @grid: a double pointer to an array
+ * @height: The height of the array
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
+	free(grid);
 }
