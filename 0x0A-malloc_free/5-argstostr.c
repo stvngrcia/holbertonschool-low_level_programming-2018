c5c9d7fd8b35268b3f22651b9f5f7b5f2efb1b8a
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-10 17:07:20 -0700

    Concatenates all the arguments of your program.

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,1 +1,57 @@
-char *argstostr(int ac, char **av);
+#include <stdlib.h>
+#include <stdio.h>
+int length(int ac, char **av);
+
+/**
+ * argstostr -  Concatenates all the arguments of your program.
+ * @ac: Counter for arguments
+ * @av: Array of values
+ * Return: A pointer to a string
+ */
+char *argstostr(int ac, char **av)
+{
+	int len;
+	int i;
+	int x;
+	int j;
+	char *heap_array;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	len = length(ac, av);
+	heap_array = malloc(len + 1);
+
+	for (x = 0, i = 0; i < ac; i++, x++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, x++)
+		{
+			heap_array[x] = av[i][j];
+		}
+		heap_array[x] = '\n';
+	}
+	heap_array[x] = '\0';
+	return (heap_array);
+}
+
+/**
+ * length -  Calcualtes the length
+ * @ac: Counter for arguments
+ * @av: Array of values
+ * Return: A the total amount of characters on the string
+ */
+int length(int ac, char **av)
+{
+	int i;
+	int j;
+	int counter;
+
+	counter = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			counter++;
+		}
+	}
+	return (counter + ac);
+}
