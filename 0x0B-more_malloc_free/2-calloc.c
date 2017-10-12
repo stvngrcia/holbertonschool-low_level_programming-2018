b953cd11dc0fc8c6c4c1c59b32cd73f2a39758ac
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-12 14:52:13 -0700

    test

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (heap_array == NULL)
 		return (NULL);
 
-	for (i = 0;  i < size; i++)
+	for (i = 0;  i < nmeb * size; i++)
 		heap_array[i] = 0;
 
 	return ((void*)heap_array);
