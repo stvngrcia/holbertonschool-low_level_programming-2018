d3e1a5fca38978c26cfda05f527948be88cfe9d6
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-12 10:04:02 -0700

    adding comments

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
-
+/**
+ * malloc_checked - Allocating with malloc
+ * @b: Amount of bytes that need to be allocated
+ * Return: a pointer to a void
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *heap_array;
