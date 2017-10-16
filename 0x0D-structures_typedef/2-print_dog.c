6503e590d7d6652186c890fe718a539a13d73092
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-16 13:53:39 -0700

    checking for null

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,5 +8,9 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
