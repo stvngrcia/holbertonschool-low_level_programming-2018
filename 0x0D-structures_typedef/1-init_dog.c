9c2ee13036d07f9d772a70a7ff9e9e122895d8a5
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-16 13:42:23 -0700

    returning early if null

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: Pointer to struct
@@ -8,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (*d == NULL)
+	if (d == NULL)
 		return;
 	d->name = name;
 	d->age = age;
