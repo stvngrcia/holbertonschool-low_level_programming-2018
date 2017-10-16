a83b8dec4804bb8722452f7c8a1fc9d09e8e1af8
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-16 12:07:24 -0700

    creating a struct for a dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLB
+#define HOLB
+
+/**
+ * struct dog - Defines a dogs attributes
+ * @name: A string for the dogs name
+ * @age: An integer for the dogs age
+ * @owner: A string for the owners name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
