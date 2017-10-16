b2c122d83b0789d08be7748c40c1d294323f55ec
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-16 14:10:57 -0700

    Creates a new instance of a struct

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new instance of a struct
+ * @name: A string representing the name of a dog
+ * @age: An integer representing then age of a dog
+ * @owner: A string representing the name of a dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
