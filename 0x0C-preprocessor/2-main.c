3d50ea46995b3c8d8c1d4d7999dbe024810b1683
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-16 10:20:59 -0700

    Prints the name of the file that it was compiled from.

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Definition: Prints the name of the file that it was compiled from
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
