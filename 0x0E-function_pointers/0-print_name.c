57dc9ab16749d0eefb5c37300166e611c3d12e7f
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-18 09:36:04 -0700

    Calls a function to print a name

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - Calls a function to print a name
+ * @name: A pointer to a string
+ * @f: A pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef HOLB
+#define HOLB
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
