405f55d89c4f689ffebbbb4bfac973f2f5cdbb6f
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-18 14:17:58 -0700

    using strcmp

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * get_op_func - Finds the right operation
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int a, int b)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
