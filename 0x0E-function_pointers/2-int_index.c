fac869caf1fa12a1d345168cb286c5299dfecc4a
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-18 13:50:42 -0700

    checking for NULL

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
