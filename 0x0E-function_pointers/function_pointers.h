b6e2dae00c208548978b2ff6bf949439b09bbf39
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-18 12:07:24 -0700

    including library

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef HOLB
 #define HOLB
 
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
