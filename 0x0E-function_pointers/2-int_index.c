9db27a1e7034eeb4d43be8a943518d3830107540
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-18 09:56:51 -0700

    Searches for an integer index on an array

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - Searches for an integer index on an array
+ * @array: Array containing the values
+ * @size: Size of the array
+ * @cmp: Pointer to a function
+ * Return: -1 if not index found or size is 0 or less. Index number when
+ * number is found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
