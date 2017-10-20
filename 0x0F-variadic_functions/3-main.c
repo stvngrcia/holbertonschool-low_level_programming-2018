81ae2babd87bf55efd53ddbcce4a6f635f19b9bf
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-20 14:28:24 -0700

    creating a flag to not print , when not needed

--- 0x0F-variadic_functions/3-main.c
+++ 0x0F-variadic_functions/3-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_all(NULL, 'H', 0, NULL);
+    print_all("ceis", 'H', 0, "lberton");
     return (0);
 }
--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -8,6 +8,7 @@
 void print_all(const char * const format, ...)
 {
 	int i;
+	int flag;
 	char *str;
 	va_list a_list;
 
@@ -19,23 +20,28 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'i':
 				printf("%i", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'f':
 				printf("%f", va_arg(a_list, double));
+				flag = 0;
 				break;
 			case 's':
 				str = va_arg(a_list, char*);
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
+				flag = 0;
 				break;
 			default:
+				flag = 1;
 				break;
 		}
-		if (format[i + 1] != '\0')
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
