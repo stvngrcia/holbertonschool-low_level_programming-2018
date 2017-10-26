6497c86d11587b225a9c29496e763f9c9b2f874b
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-26 14:21:36 -0700

    testing with two printf staments

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void myStartupFun(void)__attribute__ ((constructor));
  */
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
