997b02f8a1eb4e835f14d3d491917536ab20713e
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-26 10:47:44 -0700

    Adding function that frees lists

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - Frees a list of nodes
+ * @head: pointer to the first element of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
