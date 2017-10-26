80d5f149663e9a2d457543887670b2a80ac71382
Steven Garcia <steven.garcia@holbertonschool.com>
2017-10-26 10:35:16 -0700

    checking for NULL on pointer passed

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -10,6 +10,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	if (head == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
