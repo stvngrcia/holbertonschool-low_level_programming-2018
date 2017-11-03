dd8ef147ffd5870e6f4b5ce3bc2cd3866ba7e803
Steven Garcia <steven.garcia@holbertonschool.com>
2017-11-02 18:51:07 -0700

    creating a function that returns an index in a binary number

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * get_bit - Returns a value at a given index
+ * @n: Number to check the value of
+ * @index: The index to look for the number
+ * Return: A value at a given index or -1 if an error occours
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > (sizeof(8) * 8))
+		return (-1);
+	/*iterating number to the right index*/
+	for (i = 0; i < index; i++)
+		n = n >> 1;
+	return ((n & 1));
+}
