1757e7dd0017ee6e9de847628e224165851416c5
Steven Garcia <steven.garcia@holbertonschool.com>
2017-11-02 20:15:17 -0700

    checking for NULL

--- 0x13-bit_manipulation/4-clear_bit.c
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: Pointer to a number
@@ -10,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	mask = 1;
 	mask = mask << index;
-	if (index > sizeof(unsigned int) * 8)
+	if (index > sizeof(unsigned int) * 8 || n == NULL)
 		return (-1);
 	if (((*n >> index) & 1) == 1) /*Checks if 1 at position*/
 		*n = mask ^ *n;
