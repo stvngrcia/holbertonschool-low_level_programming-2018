1bfbf28af8b0bc18830e1eba3e7ae431bbaaf482
Steven Garcia <steven.garcia@holbertonschool.com>
2017-11-07 12:19:35 -0800

    create_file - Function that creates a file.

--- 0x14-file_io/README.md
+++ 0x14-file_io/README.md
@@ -3,3 +3,4 @@
 Functions found in this folder are:
 
 + read_textfile - Reads a file and prints it to the POSIX stdout.
++ create_file - Function that creates a file.
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -7,7 +7,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
