9c9f2c6acb910e5dfc28b7a75f0793835488fdcf
Steven Garcia <steven.garcia@holbertonschool.com>
2017-11-07 10:42:07 -0800

    read_textfile - Reads a file and prints it to the POSIX stdout.

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * read_textfile - Reads a file and prints it to the POSIX stdout.
+ * @filename: The name of the file that needs to be read.
+ * @letters: Is the number of letters the function should print.
+ * Return: The actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int i;
+	int read_letters;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read_letters = read(fd, buf, letters);
+	if (read_letters == -1)
+		return (0);
+	for (i = 0; i < read_letters; i++)
+	{
+		if (write(1, &buf[i], 1) == -1)
+			return (0);
+	}
+	close(fd);
+	free(buf);
+	return (i);
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,5 @@
+#FILE I/O
+
+Functions found in this folder are:
+
++ read_textfile - Reads a file and prints it to the POSIX stdout.
