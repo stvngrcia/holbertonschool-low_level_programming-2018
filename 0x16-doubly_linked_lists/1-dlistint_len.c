bd4e37b94c0641a19fd2cbc17c8c16f6101768da
stvngrcia <stevngrcia@yahoo.com>
2017-12-08 14:30:47 +0000

    calculating the len of a linked list

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts all the elements in a dlistint_t list.
+ * @h: Pointer to the head node of a linked list.
+ * Return: Number of nodes on the list.
+ */
+ size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
