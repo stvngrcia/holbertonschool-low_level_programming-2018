903949981359416f1bcfa1f6c43f778d5a5383c1
stvngrcia <stevngrcia@yahoo.com>
2017-12-08 14:33:28 +0000

    fixing betty stlying

--- 0x16-doubly_linked_lists/1-dlistint_len.c
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * @h: Pointer to the head node of a linked list.
  * Return: Number of nodes on the list.
  */
- size_t dlistint_len(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
 
