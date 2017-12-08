2058a2516c42d7dba5bbf1f21d14868c96ad402a
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-08 08:30:32 -0800

    adding prototypes for functions create node and adding node to the end of a list

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -24,6 +24,8 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *create_node(const int n);
 
 
 #endif /*_DLIST_*/
