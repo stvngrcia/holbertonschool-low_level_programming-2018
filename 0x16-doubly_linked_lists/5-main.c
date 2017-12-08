9caf261ec83cc190a7f2a2268defb15086d038c9
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-08 09:17:19 -0800

    test function for getting the node at nth position

---
+++ 0x16-doubly_linked_lists/5-main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+    dlistint_t *node;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    node = get_dnodeint_at_index(head, 7);
+    if (node != NULL)
+        printf("%d\n", node->n);
+    else
+        printf("NILL\n");
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
