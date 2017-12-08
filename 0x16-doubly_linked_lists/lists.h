2f696171a42b976a714fdedfa37ea9d229780b09
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-08 13:27:35 -0800

    adding prototypes for deleting node functions

--- 0x16-doubly_linked_lists/lists.h
+++ 0x16-doubly_linked_lists/lists.h
@@ -31,6 +31,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 
 #endif /*_DLIST_*/
