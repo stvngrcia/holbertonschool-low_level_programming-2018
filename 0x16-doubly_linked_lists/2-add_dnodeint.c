fd45a1dea34a248bec1d3b6aad27085be23d38f6
stvngrcia <stevngrcia@yahoo.com>
2017-12-08 16:08:17 +0000

    adds a node at the beginning of a doubly linked list

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+dlistint_t *create_node(const int n);
+
+/**
+ * add_dnodeint - Adds a node to the begining of a linked list.
+ * @head: Pointer to a pointer to the first node of a linked list.
+ * @n: Integer value to add to a linked list node.
+ * Return: Upon sucess the address of the new element, otherwise NULL.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = create_node(n);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	(*head)->prev = new_node;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: Integer value to add to a linked list node.
+ * Return: Upon sucess pointer to a dlistint_t node. Otherwise NULL
+ */
+dlistint_t *create_node(const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	new_node->n = n;
+	return (new_node);
+}
