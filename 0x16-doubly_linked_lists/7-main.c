887216dd72c41d086dde055f8dff758d5dde07ea
stvngrcia <stevengarciaacuna@gmail.com>
2017-12-08 12:42:50 -0800

    adding test file

--- 0x16-doubly_linked_lists/7-main.c
+++ 0x16-doubly_linked_lists/7-main.c
@@ -13,7 +13,7 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint_end(&head, 0);
+    /*add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
     add_dnodeint_end(&head, 3);
@@ -21,9 +21,9 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    print_dlistint(head);
+    print_dlistint(head);*/
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 1, 4096);
+    insert_dnodeint_at_index(&head, 6, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
