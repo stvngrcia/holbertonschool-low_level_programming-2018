e317bb694e725be5cea2edc6a95973058198c5b9
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-11 14:55:36 -0800

    printing all the hash table key and values

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: Pointer to the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	int flag;
+	unsigned long int i;
+	hash_node_t *tmp;
+
+	flag = 0;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while(tmp != NULL)
+		{
+			if (flag == 0)
+				flag = 1;
+			else
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
