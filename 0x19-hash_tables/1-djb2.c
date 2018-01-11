a44aa570252befd6977ffafdda02652104449972
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-11 09:59:06 -0800

    adding djb2 hashing algorithm

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,15 @@
+/**
+ * hash_djb2 - Hashing algorithm refrenced from cse.yorku.ca/~oz/hash.html
+ * @str: string that is going to be hashed
+ * Returns: An unsigned int representing the hash.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return (hash);
+}
