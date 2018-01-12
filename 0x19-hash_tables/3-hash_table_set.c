9643a42a6aba915a13ca81761c3bd9f5a23547d7
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-11 19:34:10 -0800

    using strcpy to copy strings

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdio.h>
 /**
  * hash_table_set - Adds an element to a hash table.
  * @ht: The hash table you want to add or update the key/value
@@ -8,22 +9,28 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int k_size;
+	int str_size;
 	hash_node_t *hash_node;
 	unsigned long int index;
 	const unsigned char *u_key;
+	char *str_key;
+	char *str_value;
 
 	u_key = (const unsigned char *)key;
-	k_size = check_key_size(key);
-	if (k_size == 0)
+	str_size = check_key_size(key);
+	if (str_size == 0)
 		return (0);
-
+	str_key = malloc(sizeof(char) * str_size + 1);
+	str_size = check_value_size(value);
+	str_value = malloc(sizeof(char) * str_size + 1);
+	if (str_key == NULL || str_value == NULL)
+		return (0);
 	hash_node = malloc(sizeof(hash_node_t));
 	if (hash_node == NULL)
 		return (0);
 	index = key_index(u_key, ht->size);
-	hash_node->key = (char *)key;
-	hash_node->value = (char *) value;
+	hash_node->key = strcpy(str_key, key);
+	hash_node->value = strcpy(str_value, value);
 	hash_node->next = ht->array[index];
 	ht->array[index] = hash_node;
 	return (1);
@@ -39,10 +46,21 @@ int check_key_size(const char *key)
 {
 	int i;
 
-	for (i = 0; key != '\0'; i++)
-	{
-		if (i > 0)
-			return (i);
-	}
+	for (i = 0; key[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * check_key_size - Checks the key size.
+ * @key: pointer to an array of characters.
+ * Return: 0 if empty string. Otherwise 1.
+ */
+int check_value_size(const char *value)
+{
+	int i;
+
+	for (i = 0; value[i] != '\0'; i++)
+		;
 	return (i);
 }
