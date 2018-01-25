13c70d87bb41b32e0ffd955b7366d78ff2a1dd6c
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-24 17:44:50 -0800

    setting up the files for insertion sort algorithm

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the insertion sort algorithm
+ * @list: Pointer to a pointer to the head of the list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
