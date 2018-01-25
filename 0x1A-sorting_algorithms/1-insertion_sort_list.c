428f32ac97864a468c937c21f088960212ecff3a
Steven Garcia <27@p3-4.hbtn.io>
2018-01-25 14:50:16 -0800

    implementing insertion sort in a linked list

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -7,5 +7,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *key;
+	listint_t *tmp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	key = (*list)->next;
+
+	while (key != NULL)
+	{
+		if (key->next->n < key->n)
+		{
+			tmp = key->next;
+			key->prev->next = tmp;
+			tmp->next->prev = key;
+			key->next = tmp->next;
+			tmp->prev = key->prev;
+			tmp->next = key;
+			key->prev = tmp;
+		}
+		else
+			key = key->next;
+		while (tmp->prev != NULL && tmp->prev->n > tmp->n)
+		{
+			tmp->prev->next = key;
+			key->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			key->prev->prev = tmp;
+		}
+	}
 }
