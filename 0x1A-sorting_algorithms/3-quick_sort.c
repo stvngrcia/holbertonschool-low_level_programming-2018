686dc1f072ad187dc11e832b92b76114c3071798
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-27 16:47:38 -0800

    separating the swapping functionality from the sorting part

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 #include <stdlib.h>
 void sort(int *array, size_t low, size_t size, size_t);
+size_t swap(int *array, size_t ptr1, size_t ptr2, size_t pivot, size_t og);
 
 /**
  * quick_sort - Sorts an array of integers in asceding order using the
@@ -37,6 +38,31 @@ void sort(int *array, size_t low, size_t size, size_t og)
 	ptr2 = pivot - 1;
 	if (pivot == 0 || size == 0 || ptr2 <= ptr1)
 		return;
+	ptr1 = swap(array, ptr1, ptr2, pivot, og);
+	if (array[pivot] < array[ptr1])
+	{
+		tmp = array[pivot];
+		array[pivot] = array[ptr1];
+		array[ptr1] = tmp;
+		print_array(array, og);
+	}
+	sort(array, low, ptr1, og);
+	sort(array, ptr1, size, og);
+}
+
+/**
+ * swap - Swaps the numbers in the array.
+ * @array: Array of integers that need to be sorted.
+ * @ptr1: The lower bound of the array partition.
+ * @ptr2: The upper bound of the array partion.
+ * @pivot: The position used to compared the numbers against.
+ * @og: The original size of the whole array. Used only for printing.
+ * Return: The value of ptr1 which tells where the pivot should go.
+ */
+size_t swap(int *array, size_t ptr1, size_t ptr2, size_t pivot, size_t og)
+{
+	int tmp;
+
 	while (ptr1 != ptr2)
 	{
 		if (array[ptr1] > array[pivot] && array[ptr2] < array[pivot])
@@ -58,13 +84,5 @@ void sort(int *array, size_t low, size_t size, size_t og)
 				ptr2--;
 		}
 	}
-	if (array[pivot] < array[ptr1])
-	{
-		tmp = array[pivot];
-		array[pivot] = array[ptr1];
-		array[ptr1] = tmp;
-		print_array(array, og);
-	}
-	sort(array, low, ptr1, og);
-	sort(array, ptr1, size, og);
+	return (ptr1);
 }
