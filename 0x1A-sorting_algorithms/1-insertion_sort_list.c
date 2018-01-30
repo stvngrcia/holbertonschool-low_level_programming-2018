07ca75782ca43007ea12caa91b8ad6e9ce0575bb
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-29 16:46:22 -0800

    adding a new and imporved insertion sorting algorithm

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-
+listint_t *swap(listint_t **current, listint_t **sorted);
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending
  * order using the insertion sort algorithm
@@ -7,44 +7,67 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *key;
-	listint_t *tmp;
+	listint_t *current;
+	listint_t *sorted;
+
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	key = (*list)->next;
-	while (key != NULL)
+
+	sorted = *list;
+	current = (*list)->next;
+
+	while (current != NULL)
 	{
-		if (key->next != NULL && key->next->n < key->n)
+		if (sorted != NULL &&  current->n < sorted->n)
 		{
-			tmp = key->next;
-			key->prev->next = tmp;
-			if (tmp->next != NULL)
-				tmp->next->prev = key;
-			key->next = tmp->next;
-			tmp->prev = key->prev;
-			tmp->next = key;
-			key->prev = tmp;
+
+			current = swap(&current, &sorted);
+			sorted = current->prev;
+			if (sorted == NULL)
+				*list = current;
 			print_list(*list);
+
+			while (sorted != NULL && current->n < sorted->n)
+			{
+				current = swap(&current, &sorted);
+				sorted = current->prev;
+				if (sorted == NULL)
+					*list = current;
+				print_list(*list);
+			}
 		}
 		else
 		{
-			tmp = key;
-			key = key->next;
-		}
-		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
-		{
-			tmp->prev->next = tmp->next;
-			tmp->next = tmp->prev;
-			if (tmp->prev->prev != NULL)
-				tmp->prev->prev->next = tmp;
-			tmp->prev = tmp->prev->prev;
-			tmp->next->prev = tmp;
-			if (tmp->next->next != NULL)
-				tmp->next->next->prev = tmp->next;
-			if (tmp->prev == NULL)
-				*list = tmp;
-			print_list(*list);
+			sorted = current;
+			current = sorted->next;
 		}
 	}
+}
+
+/**
+ * swap - Swaps the numbers in the array to put them in order
+ * @current: Pointer to a pointer to a node in the linked list
+ * @sorted: Pointer to a pointer to a node in the linked list
+ * Return: A pointer to the current node.
+ */
+listint_t *swap(listint_t **current, listint_t **sorted)
+{
+	listint_t *tmp;
+
+	tmp = (*current)->next;
+	if (*sorted != NULL)
+		(*sorted)->next = tmp;
+	if (tmp != NULL)
+	{
+		tmp->prev = *sorted;
+	}
+	if (*current != NULL)
+		(*current)->next = *sorted;
+		(*current)->prev = (*sorted)->prev;
+	if ((*sorted)->prev != NULL)
+		(*sorted)->prev->next = *current;
+	((*sorted)->prev) = *current;
+
+	return (*current);
 }
