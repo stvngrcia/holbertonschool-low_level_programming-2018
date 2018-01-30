bf314a6a9118558593ea63ca86288364cf193c5e
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-30 10:08:42 -0800

    testing implementation

--- 0x1A-sorting_algorithms/107-main.c
+++ 0x1A-sorting_algorithms/107-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {87, 65, 28, 63, 93, 52, 39, 59, 27, 30, 24, 83, 69, 62, 13, 6, 88, 58, 92, 26, 42, 11, 16, 21, 75, 36, 71, 8, 45, 38};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
