bf020ba74851c566a83d2fbcb77b1c3129862c00
stvngrcia <stevengarciaacuna@gmail.com>
2018-01-31 14:56:04 -0800

    using flags to check when the list is sorted already

--- 0x1A-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1A-sorting_algorithms/101-cocktail_sort_list.c
@@ -1,95 +1,72 @@
 #include "sort.h"
-void swap(listint_t **list, listint_t **current, int, listint_t **up);
+int swap(listint_t **list, listint_t *current, int);
+
 /**
- *
- *
- *
- *
+ * cocktail_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Cocktail shaker sort algorithm.
+ * @list: Pointer to a pointer to the first node of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
 	int flag;
-	listint_t *up;
-	listint_t *low;
-	listint_t *current;
+	listint_t *tmp;
 
-	low = *list;
-	current = low;
-	flag = 1;
-
-	for(up = *list; up->next != NULL;)
-		up = up->next;
-
-	while (low != up)
+	flag = 0;
+	tmp = *list;
+	if (list == NULL || *list == NULL)
+		return;
+	while (flag == 0)
 	{
-		while(current != up)
+		flag = 1;
+		while (tmp->next != NULL)
 		{
-			if (current->n > current->next->n)
-			{
-				flag = 0;
-				swap(list, &current, 0, &up);
-				print_list(*list);
-			}
-
+			if (tmp->n > tmp->next->n)
+				flag = swap(list, tmp, 0);
 			else
-				current = current->next;
-			if (current->prev == up)
-			{
-				up = current;
-				printf("hello");
-				printf("--- %d ---\n", up->n);
-			}
-
+				tmp = tmp->next;
 		}
-		if (flag == 0)
-		{
-			up = up->prev;
-			flag = 1;
-		}
-		printf("--- %d ---\n", up->n);
-		current = up;
-		while(current != low)
-		{
-			if (current->n < current->prev->n)
-			{
-				swap(list, &current, 1, &up);
-				print_list(*list);
-			}
+		tmp = tmp->prev;
 
+		while (tmp->prev != NULL)
+		{
+			if (tmp->n < tmp->prev->n)
+				flag = swap(list, tmp, 1);
 			else
-				current = current->prev;
-			if (current->next == low)
-				low = current;
+				tmp = tmp->prev;
 		}
-		low = low->next;
-		current = low;
 	}
 }
 
-void swap(listint_t **list, listint_t **current, int flag, listint_t **up)
+/**
+ * swap - Swaps two items in a linked list.
+ * @list: Pointer to a pointer to the first item of the list.
+ * @current: Pointer to a pointer to the current node.
+ * @flag: 1 when sorting backwards in the list. 0 When is sorting forward.
+ * Return: Always zero.
+ */
+int swap(listint_t **list, listint_t *current, int flag)
 {
 	listint_t *tmp;
 
 	if (flag == 1)
-		*current = (*current)->prev;
-	tmp = (*current)->next;
+		current = current->prev;
+	tmp = current->next;
 
 	if (tmp != NULL)
 	{
-		(*current)->next = tmp->next;
-		tmp->next = *current;
-		tmp->prev = (*current)->prev;
+		current->next = tmp->next;
+		tmp->next = current;
+		tmp->prev = current->prev;
 	}
-	if ((*current)->next != NULL)
-		(*current)->next->prev = *current;
-	if ((*current)->prev != NULL)
-		(*current)->prev->next = tmp;
-	(*current)->prev = tmp;
+	if (current->next != NULL)
+		current->next->prev = current;
+	if (current->prev != NULL)
+		current->prev->next = tmp;
+	current->prev = tmp;
 
 	if (tmp->prev == NULL)
 		*list = tmp;
 
-	if (tmp == *up)
-		*up = *up;
+	print_list(*list);
+	return (0);
 }
-
