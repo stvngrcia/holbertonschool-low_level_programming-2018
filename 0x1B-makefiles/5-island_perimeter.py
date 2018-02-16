915b826e800bfcce69b02b48986a0269cd2fe414
stvngrcia <stevengarciaacuna@gmail.com>
2018-02-16 12:14:47 -0800

    calculating the perimeter of an island represented by a grid

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    '''
+        Calculates the perimeter of an island that is surrounded by water
+        the Land is represented by 1 and the water is by 0.
+        Aguments:
+            - grid: A matrix filled with 0, 1 representing a map.
+    '''
+    water = 0
+    land = 1
+    perimeter = 0
+
+    for y, level in enumerate(grid):
+        for x, parcel in enumerate(level):
+            if parcel == land:  # Checking only when hitting land
+                if grid[y][x - 1] == water:  # Looking west
+                    perimeter += 1
+                if grid[y][x + 1] == water:  # Looking east
+                    perimeter += 1
+                if grid[y - 1][x] == water:  # Looking north
+                    perimeter += 1
+                if grid[y + 1][x] == water:  # Looking south
+                    perimeter += 1
+
+    return perimeter
