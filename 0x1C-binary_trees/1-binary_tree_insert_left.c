bb80c7b326297534545ee1e7505c7be447759d30
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-05 17:39:25 -0800

    modifying the pointer to left child parent

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		new_node->left = parent->left;
 		parent->left = new_node;
+		new_node->left->parent = new_node;
 	}
-
 	return (new_node);
 }
