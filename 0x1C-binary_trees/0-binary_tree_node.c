fd919157962078cac1befa5df60dd8b94797466e
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-05 17:40:31 -0800

    adding documentation for return value

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_node - Creates a binary tree node
  * @parent: Pointer to the parent node
  * @value: Value to be added to the node
+ * Return: If sucessful a pointer to the new node. Otherwise NULL
  **/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
