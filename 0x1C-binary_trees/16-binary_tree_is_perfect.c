7319b60e5cbb60e7f88e346e604e76bccbd274e3
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-07 13:11:52 -0800

    separating the parent call into left and right

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-int is_perfect(const binary_tree_t *h, const binary_tree_t *tree, int size);
+int is_perfect(const binary_tree_t *tree, int size);
 
 /**
  * binary_tree_is_perfect - Checks if a tree if perfect.
@@ -8,14 +8,21 @@ int is_perfect(const binary_tree_t *h, const binary_tree_t *tree, int size);
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int result;
+	int right;
+	int left;
 	int size;
 
 	if (tree == NULL)
 		return (0);
 	size = 0;
-	result = is_perfect(tree, tree, size);
-	return (result);
+	left = is_perfect(tree->left, size);
+	right = is_perfect(tree->right, size);
+
+	if (right != left)
+		return (0);
+	if (right < 0 || left < 0)
+		return (0);
+	return (1);
 }
 
 /**
@@ -25,7 +32,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  * @size: size of the tree.
  * Return: 1 if the tree is perfect. Otherwise 0.
  */
-int is_perfect(const binary_tree_t *h, const binary_tree_t *tree, int size)
+int is_perfect(const binary_tree_t *tree, int size)
 {
 	int right;
 	int left;
@@ -36,21 +43,14 @@ int is_perfect(const binary_tree_t *h, const binary_tree_t *tree, int size)
 	if (tree == NULL)
 		return (0);
 
-	left = left + is_perfect(h, tree->left, left);
-	right = right + is_perfect(h, tree->right, right);
+	left = left + is_perfect(tree->left, left);
+	right = right + is_perfect(tree->right, right);
 	if (tree->left == NULL && tree->right != NULL)
 		return (-1);
 	if (tree->left != NULL && tree->right == NULL)
 		return (-1);
 	if (right < 0 || left < 0)
 		return (-1);
-	if (tree == h)
-	{
-		if (right != left)
-			return (0);
-		else
-			return (1);
-	}
 	if (right > left)
 		return (right + 1);
 	return (left + 1);
