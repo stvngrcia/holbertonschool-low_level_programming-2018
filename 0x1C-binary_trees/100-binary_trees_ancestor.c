9001a02dfc44a796aa3ab6ca7d7d3acf41753362
stvngrcia <stevengarciaacuna@gmail.com>
2018-03-07 11:00:51 -0800

    finding the lowest common ancestor

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -1,29 +1,31 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: The first node to check.
+ * @second: The second node to check.
+ * Return: A pointer to the lowest ancestor or NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
-	binary_tree_t *tmp;
+	binary_tree_t *tmp1;
+	binary_tree_t *tmp2;
 
 	if (first == second)
 		return ((binary_tree_t *)first);
 	if (first->parent == second->parent)
 		return (first->parent);
 
-	tmp = (binary_tree_t *)first;
-
-	while(tmp != NULL)
+	tmp1=(binary_tree_t *)first;
+	for (; tmp1 != NULL; tmp1=tmp1->parent)
 	{
-		if (tmp == second->parent)
-			return (tmp);
-		tmp = tmp->parent;
-	}
-
-	tmp = (binary_tree_t *)second;
-	while(tmp != NULL)
-	{
-		if (tmp == first->parent)
-			return(tmp);
-		tmp = tmp->parent;
+		tmp2=(binary_tree_t *)second;
+		for(; tmp2 != NULL; tmp2=tmp2->parent)
+		{
+			if (tmp1 == tmp2)
+				return (tmp1);
+		}
 	}
 	return (NULL);
 }
