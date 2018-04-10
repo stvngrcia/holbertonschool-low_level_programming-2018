56ec79db4a165cd554dc6afaafe912495018d3cc
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-10 08:46:12 -0700

    adding linear search algo file

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_search - Looks for a value in an array
+ * @array: list of integers
+ * @size: The size of the array
+ * @value: The value that the algorithm is trying to find
+ * Return: The index in which the value is located in the array. If the
+ * value is not found -1 is return.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t idx;
+
+	if (array == NULL)
+		return (-1);
+
+	for (idx = 0; idx <= size; idx++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
+
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH
+#define SEARCH
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+
+#endif
