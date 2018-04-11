2e5564185c7be1003541a32028ad3f9992bb3b1b
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-11 13:41:12 -0700

    testing

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -16,12 +16,11 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (idx = 0; idx <= size; idx++)
+	for (idx = 0; idx <= size - 1; idx++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
 		if (array[idx] == value)
 			return (idx);
-
 	}
 	return (-1);
 }
