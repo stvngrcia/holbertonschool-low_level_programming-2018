b47f6c049d611bb96e2f1aa8043608031904fa85
stvngrcia <stevengarciaacuna@gmail.com>
2018-04-13 07:20:14 -0700

    checking for null in initial function

--- 0x1D-search_algorithms/103-exponential.c
+++ 0x1D-search_algorithms/103-exponential.c
@@ -15,7 +15,7 @@ int exponential_search(int *array, size_t size, int value)
 	size_t idx;
 	int low;
 
-	if (array == NULL || size = 0)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	for (idx = 1; idx < size; idx = idx * 2)
